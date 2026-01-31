51572ede559cfce45a98cb019d11094f1a426cc5
Kavindu Gihan <kavidudharmasiri90@gmail.com>
2026-01-31 18:33:20 +0530

    base url changed

--- src/base_url.js
+++ src/base_url.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const admin_axios = axios.create({
-  baseURL: "http://127.0.0.1:5000/",
+  baseURL: "https://audio-backend.kavindu-gihan.live/",
 });
 
+// http://127.0.0.1:5000/
 export default admin_axios;
